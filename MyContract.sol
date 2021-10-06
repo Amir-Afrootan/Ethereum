264aee23473057fa23ae20895498c49c5cb59e91
Amir-Afrootan <31209648+Amir-Afrootan@users.noreply.github.com>
2021-10-06 13:47:14 +0330

    Update MyContract.sol

--- MyContract.sol
+++ MyContract.sol
@@ -1,47 +1,53 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.4;
+pragma solidity >=0.7.0 <0.9.0;
 
-contract Coin {
-    // new git test
-    // The keyword "public" makes variables
-    // accessible from other contracts
-    address minter;
-    mapping(address => uint256) public balances;
-
-    // Constructor code is only run when the contract
-    // is created
-    constructor() {
-        minter = msg.sender;
-        //minter = _wallet;
+contract Users
+{
+    // properties
+    uint256 id = 0;
+    string name = "Amir";
+    mapping(uint => string) public MyRoles;
+    struct Contact
+    {
+        uint256 ContactId;
+        string mobile;
     }
+    Contact[] MyContact;
 
-    // Sends an amount of existing coins
-    // from any caller to an address
-    function send(address receiver, uint256 amount) public {
-        if (amount > balances[msg.sender])
-            revert InsufficientBalance({
-                requested: amount,
-                available: balances[msg.sender]
-            });
-
-        balances[msg.sender] -= amount;
-        balances[receiver] += amount;
-        emit Sent(msg.sender, receiver, amount);
+    // constructor
+    constructor (uint256 i, string memory n)
+    {
+        id = i;
+        name = n;
     }
-
-    // Events allow clients to react to specific
-    // contract changes you declare
-    event Sent(address from, address to, uint256 amount);
-
-    // Sends an amount of newly created coins to an address
-    // Can only be called by the contract creator
-    function mint(address receiver, uint256 amount) public {
-        require(msg.sender == minter);
-        balances[receiver] += amount;
+    
+    function GetName() public view returns (string memory)
+    {
+        return name;
     }
-
-    // Errors allow you to provide information about
-    // why an operation failed. They are returned
-    // to the caller of the function.
-    error InsufficientBalance(uint256 requested, uint256 available);
+    
+    function SetName (string memory n) public
+    {
+        name = n;
+    }
+    
+    // working with struct
+    function GetContact(uint256 i) public view returns (Contact memory)
+    {
+        return MyContact[i];
+    }
+    function SetContact(uint256 i, string memory m) public
+    {
+        Contact memory temp;
+        temp.ContactId = i;
+        temp.mobile = m;
+        MyContact.push(temp);
+        //MyContact.push({"ContactId":i, "mobile":m});
+    }
+    
+    // working with mapping
+    function SetRole (uint256 i, string memory r) public
+    {
+        MyRoles[i] = r;
+    }
 }
